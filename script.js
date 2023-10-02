d7b86786f7b42f2b7309588c4b2efcda2a8768af
dahora <rodrigoliveira328@hotmail.com>
2023-10-02 09:08:20 -0300

    Alter avatar

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/rodrigo-light.jpeg")
+    img.setAttribute("src", "./assets/rodrigo-light.png")
     img.setAttribute("alt", "A mensagem foi alterada")
   } else {
-    img.setAttribute("src", "./assets/rodrigo.jpg")
+    img.setAttribute("src", "./assets/rodrigo.png")
     img.setAttribute("alt", "A mensagem foi não alterada")
   }
 }
